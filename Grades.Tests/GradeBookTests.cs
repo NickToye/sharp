56b6743a071f42925a823a24c49a07ba1433c759
Nick Toye <nick@pixelsandcode.co.uk>
2018-07-13 16:13:54 +0100

    Initial check-in of module Grades

---
+++ Grades.Tests/GradeBookTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xunit;
+using Grades;
+
+namespace Grades.Tests
+{
+    public class GradeBookTests
+    {
+        [Fact]
+        public void ComputesHighestGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(10);
+            book.AddGrade(90);
+
+            GradeStatistics result = book.ComputeStatistics();
+            Assert.Equal(90, result.HighestGrade);
+        }
+    }
+}
---
+++ Grades.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace Grades.Tests
+{
+
+
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            Assert.Equal(3, 3);
+        }
+    }
+}
